d71d93c7515cb4052861cc009517766c667c3fd3
tigistyitayew <chinayitayew@gmail.com>
2022-12-22 12:14:01 -0800

    starat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,28 @@
-@include "main.h"
+#include "main.h"
 
-char *_strcat(char *dest, char *src){
-	int index =0 , dest_len =0;
-	while (dest[index++])
-		dest_len++;
-	for (index =0 ; src[index]; index++)
-		dest[dest_len++]=src[index];
+/**
+ * _strcat - concatennats two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
