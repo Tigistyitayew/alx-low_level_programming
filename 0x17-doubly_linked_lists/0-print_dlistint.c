f8be5f9070a8470ac622226c710d223b13c2f398
tigistyitayew <chinayitayew@gmail.com>
2024-01-11 11:32:36 -0800

    doubly

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints length and elements
- * @h: head of list
- * Return: size_t value
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *tmp;
-	int count = 0;
+	size_t count = 0;
 
-	tmp = h;
-	while (tmp != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,30 +1,29 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
+ * sum_dlistint - returns the sum of all the data (n)
+ * of a doubly linked list
  *
- * Return: Always EXIT_SUCCESS.
+ * @head: head of the list
+ * Return: sum of the data
  */
-int main(void)
+int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *head;
 	int sum;
 
-	head = NULL;
-	add_dnodeint_end(&head, 0);
-	add_dnodeint_end(&head, 1);
-	add_dnodeint_end(&head, 2);
-	add_dnodeint_end(&head, 3);
-	add_dnodeint_end(&head, 4);
-	add_dnodeint_end(&head, 98);
-	add_dnodeint_end(&head, 402);
-	add_dnodeint_end(&head, 1024);
-	sum = sum_dlistint(head);
-	printf("sum = %d\n", sum);
-	free_dlistint(head);
-	head = NULL;
-	return (EXIT_SUCCESS);
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+
+	return (sum);
 }
